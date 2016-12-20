f927fdc9a4c1ff90c1eeaef6b43ba56b3a70b9aa
benjie estrella <bench323010@gmail.com>
2016-12-20 20:52:37 +0800

    Crrated
    Signed-off-by: benjie estrella <bench323010@gmail.com>

---
+++ ObjectLifetime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            //set properties
+
+            Car myOtherCar = myCar;
+
+
+            myOtherCar = null;
+
+            myCar = null;
+        }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+            public double originalPrice { get; set; }
+
+
+            public Car()
+            {
+                this.Make = "NISSAN";
+
+            }
+
+            public Car(String make, String model, int year, string color)
+            {
+
+                Make = make;
+                Model = model;
+                Year = year;
+                Color = color;
+
+            }
+
+        }
+    }
+}
